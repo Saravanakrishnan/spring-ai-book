a8e4a44fdc03c03352845869071d661c3ad9c5f5
Saravanakrishnan Pk <8683623+Saravanakrishnan@users.noreply.github.com>
2026-02-22 09:32:55 +0530

    feat: ch03 base version
    Signed-off-by: Saravanakrishnan Pk <8683623+Saravanakrishnan@users.noreply.github.com>

---
+++ chapter03/src/main/java/ch03/service/LightService.java
@@ -0,0 +1,45 @@
+package ch03.service;
+
+import ch03.model.Light;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LightService {
+
+    private final Logger logger = LoggerFactory.getLogger(LightService.class);
+
+    ApplicationContext context;
+
+    public LightService(ApplicationContext context) {
+        this.context = context;
+    }
+
+    public List<Light> getLights() {
+        return context
+                .getBeansOfType(Light.class)
+                .values()
+                .stream()
+                .toList();
+    }
+
+    public Optional<Light> getLight(String color) {
+        return getLights()
+                .stream()
+                .filter(light -> light.getColor().equalsIgnoreCase(color))
+                .findFirst();
+    }
+
+    public Optional<Light> setLight(String color, boolean status) {
+        var light = getLight(color);
+        light.ifPresent(it -> {
+            it.setOn(status);
+        });
+        return light;
+    }
+}
